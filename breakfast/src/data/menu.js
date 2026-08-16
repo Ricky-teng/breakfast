5246171d32911b274e04ba0385cec7a2094c4343
Ricky <tengchihchi@gmail.com>
2026-08-16 13:08:22 +0800

    Delete breakfast directory

--- breakfast/src/data/menu.js
+++
@@ -1,6 +0,0 @@
-export const MENU = [
-  { id: 'egg-toast', name_zh: '蛋吐司', name_en: 'Egg Toast', price: 35 },
-  { id: 'ham-egg-toast', name_zh: '火腿蛋吐司', name_en: 'Ham & Egg Toast', price: 45 },
-  { id: 'hash-brown', name_zh: '薯餅', name_en: 'Hash Brown', price: 25 },
-  { id: 'soy-milk', name_zh: '豆漿', name_en: 'Soy Milk', price: 20 }
-]
